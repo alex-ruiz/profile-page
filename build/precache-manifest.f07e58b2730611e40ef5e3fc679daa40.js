460668d569dcc55b9d40ea0518efb1bdce2cf902
tomas ruiz <tomas@iconos.mx>
2020-10-05 17:55:36 -0700

    build

---
+++ build/precache-manifest.f07e58b2730611e40ef5e3fc679daa40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec5d18ed1aecc91bc3a56dfee5b49e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16108e882542de2c082f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40b3e26b1be94944e40d",
+    "url": "/static/js/2.060e4ada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.060e4ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16108e882542de2c082f",
+    "url": "/static/js/main.59491304.chunk.js"
+  },
+  {
+    "revision": "40b75bf57960df7bc0e8",
+    "url": "/static/js/runtime-main.d6f6ceba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
